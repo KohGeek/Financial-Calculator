f5612fd352af203cc6ee971e09a583a27918f9c6
yhlow0314 <yhlow0314@1utar.my>
2018-10-30 15:27:46 +0800

    Update main.c
    Add mortang loan and interest module

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mortageloan();
+
 int main()
 {
+    mortageloan();
+    interest();
     printf("Hello world!\n");
 
+    return 0;
+}
+
+int mortageloan(){
+    return 0;
+}
+
+int interest(){
     return 0;
 }
