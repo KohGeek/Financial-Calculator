ed8d322a0b258a2eadcf6803e337d771099d491b
Koh Jun Dong <KohComputer@gmail.com>
2018-11-08 00:25:10 +0800

    Fix some codes
    Formatting the codes and implementing other algorithms that might be more correct for the housing loan

--- src/main.c
+++ src/main.c
@@ -1,73 +1,75 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
 #include <version.h>
-
 
 void main_menu(void);
 void housing_loan(void);
 
-int main()
+int main(void)
 {
-	int opt;
+    int opt;
 
-	printf("\t\t\t\t\t***SIMPLE FINANCIAL CALCULATOR***\n\n");
+    printf("\t\t\t\t\t***SIMPLE FINANCIAL CALCULATOR***\n\n");
 
-	do {
-		main_menu();
+    do
+    {
+        main_menu();
 
-		printf("Enter a option to continue: ");
-		scanf("%d", &opt);
+        printf("Enter a option to continue: ");
+        scanf("%d", &opt);
 
-		switch(opt)
-		{
-		case 0:
-			exit(0);
-			break;
-		case 1:
-			housing_loan();
-			break;
-		case 2:
-			//car_loan();
-			break;
-		case 3:
-			//KWSP();
-			break;
-		}
-	} while (opt != 0);
-	return 0;
+        switch(opt)
+        {
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            housing_loan();
+            break;
+        case 2:
+            //car_loan();
+            break;
+        case 3:
+            //KWSP();
+            break;
+        }
+    }
+    while (opt != 0);
+
+    return 0;
 }
 
 void main_menu(void)
 {
-	printf(" 1  Housing Loan\n");
-	printf(" 2  Car Loan\n");
-	printf(" 3  Retirement Planning (KWSP)\n");
-	printf(" 4  (To be volunteered)\n");
-	printf(" 9  Help & About\n");
-	printf(" 0  Exit\n\n\n");
+    printf(" 1  Housing Loan\n");
+    printf(" 2  Car Loan\n");
+    printf(" 3  Retirement Planning (KWSP)\n");
+    printf(" 4  (To be volunteered)\n");
+    printf(" 9  Help & About\n");
+    printf(" 0  Exit\n\n\n");
 }
 
 void housing_loan(void)
 {
-	int tenure;
-	double cost, loan_amt, balance;
-	float downpay, loan_percnt, interest, installment;
-	printf("\t\t\t\t\t***Housing Loan Calculator***\n\n\n");
-	printf("\t\tCost of House (RM): ");
-	scanf("%lf", &cost);
-	printf("\n\t\tLoan Percentage (%%): ");
-	scanf("%f", &loan_percnt);
-	printf("\n\t\tLoan Tenure (Years): ");
-	scanf("%d", &tenure);
-	printf("\n\t\tInterest Rate (%% P.A): ");
-	scanf("%f", &interest);
+    int tenure;
+    double cost, loan_amt, balance;
+    float downpay, loan_percnt, interest, installment;
+    printf("\t\t\t\t\t***Housing Loan Calculator***\n\n\n");
+    printf("\t\tCost of House (RM): ");
+    scanf("%lf", &cost);
+    printf("\n\t\tLoan Percentage (%%): ");
+    scanf("%f", &loan_percnt);
+    printf("\n\t\tLoan Tenure (Years): ");
+    scanf("%d", &tenure);
+    printf("\n\t\tInterest Rate (%% P.A): ");
+    scanf("%f", &interest);
 
-	//Need help in data truncation for higher accuracy calculation
-	loan_percnt *= 0.01; interest *= 0.01;
-	loan_amt = loan_percnt * cost;
-	installment = loan_amt * interest * tenure / (12 * tenure);
+    //Need help in data truncation for higher accuracy calculation
+    loan_percnt *= 0.01;
+    interest *= 0.01;
+    loan_amt = loan_percnt * cost;
+    //installment = loan_amt * interest * tenure / (12 * tenure);
+    installment = loan_amt * (1 + interest) / (tenure * 12);
 
-	printf("\n\t\t**Monthly repayment** : RM %.2f\n\n", installment);
+    printf("\n\t\t**Monthly repayment** : RM %.2f\n\n", installment);
 }
--- src/version.h
+++ src/version.h
@@ -2,29 +2,29 @@
 #define VERSION_H
 
 	//Date Version Types
-	static const char DATE[] = "30";
-	static const char MONTH[] = "10";
+	static const char DATE[] = "08";
+	static const char MONTH[] = "11";
 	static const char YEAR[] = "2018";
-	static const char UBUNTU_VERSION_STYLE[] =  "18.10";
+	static const char UBUNTU_VERSION_STYLE[] =  "18.11";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
 	static const char STATUS_SHORT[] =  "a";
 	
 	//Standard Version Type
-	static const long MAJOR  = 1;
+	static const long MAJOR  = 0;
 	static const long MINOR  = 0;
-	static const long BUILD  = 0;
-	static const long REVISION  = 0;
+	static const long BUILD  = 11;
+	static const long REVISION  = 63;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 1,0,0,0
-	#define RC_FILEVERSION_STRING "1, 0, 0, 0\0"
-	static const char FULLVERSION_STRING [] = "1.0.0.0";
+	static const long BUILDS_COUNT  = 18;
+	#define RC_FILEVERSION 0,0,11,63
+	#define RC_FILEVERSION_STRING "0, 0, 11, 63\0"
+	static const char FULLVERSION_STRING [] = "0.0.11.63";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 0;
+	static const long BUILD_HISTORY  = 11;
 	
 
 #endif //VERSION_H
