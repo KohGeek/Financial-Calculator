2578799439217801843aee21080fb7a1b70fef8d
Koh Jun Dong <KohComputer@gmail.com>
2018-11-08 00:27:50 +0800

    Fixing headers
    Using the correct symbol for header files instead of hacking the build options

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <version.h>
+#include "version.h"
 
 void main_menu(void);
 void housing_loan(void);
--- src/version.h
+++ src/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 0;
-	static const long BUILD  = 11;
-	static const long REVISION  = 63;
+	static const long BUILD  = 12;
+	static const long REVISION  = 68;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 18;
-	#define RC_FILEVERSION 0,0,11,63
-	#define RC_FILEVERSION_STRING "0, 0, 11, 63\0"
-	static const char FULLVERSION_STRING [] = "0.0.11.63";
+	#define RC_FILEVERSION 0,0,12,68
+	#define RC_FILEVERSION_STRING "0, 0, 12, 68\0"
+	static const char FULLVERSION_STRING [] = "0.0.12.68";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 11;
+	static const long BUILD_HISTORY  = 12;
 	
 
 #endif //VERSION_H
