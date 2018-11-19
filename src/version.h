f882c388993935020ecabe5dca0d6445d3ef7a00
Koh Jun Dong <KohComputer@gmail.com>
2018-11-20 00:44:46 +0800

    Simplify the loop again
    Apparently I overwritten some crap in my attempt to quickly fix shit

--- src/version.h
+++ src/version.h
@@ -8,17 +8,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 114;
-	static const long REVISION  = 649;
+	static const long BUILD  = 115;
+	static const long REVISION  = 651;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 207;
-	#define RC_FILEVERSION 0,1,114,649
-	#define RC_FILEVERSION_STRING "0, 1, 114, 649\0"
-	static const char FULLVERSION_STRING [] = "0.1.114.649";
+	static const long BUILDS_COUNT  = 209;
+	#define RC_FILEVERSION 0,1,115,651
+	#define RC_FILEVERSION_STRING "0, 1, 115, 651\0"
+	static const char FULLVERSION_STRING [] = "0.1.115.651";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 14;
+	static const long BUILD_HISTORY  = 15;
 	
 
 #endif //VERSION_H
