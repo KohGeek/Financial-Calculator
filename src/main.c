6f2667a17c59502ac55f03857cbfddb1185e8cd9
Koh Jun Dong <KohComputer@gmail.com>
2018-11-20 00:18:56 +0800

    Allow full looping in housing loan function

--- src/main.c
+++ src/main.c
@@ -40,13 +40,13 @@ int main()
             housing_loan();
             break;
         case 2:
-            //car_loan();
+            car_loan();
             break;
         case 3:
             //KWSP();
             break;
         case 4:
-            //bank_interest();
+            bank_interest();
             break;
         }
     }
@@ -78,97 +78,91 @@ void housing_loan(void)
     float loan_percnt, interest, installment, monthly_intrst;
 	system("cls");
 
-	do
-	{
-		printf("\n\n\t\t\t\t\t***Housing Loan Calculator***\n\n\n");
-		*_string = "\t\tCost of House (RM)\t: \0"; //test implementation of new function
-		dchecker(100000000, 1, 2, 0);
-		cost = _gfloat;
-		//printf("\t\tCost of House (RM)\t: ");
-		//scanf("%lf", &cost);
-		printf("\n\t\tLoan Percentage (%%)\t: ");
-		scanf("%f", &loan_percnt);
-		printf("\n\t\tLoan Tenure (Years)\t: ");
-		scanf("%d", &tenure);
-		printf("\n\t\tInterest Rate (%% P.A)\t: ");
-		scanf("%f", &interest);
-		do
-		{
-			printf("\n\t\tFirst installment (yy/mm) : "); //suggestion - break month and date into two scanf, might make validation easy
-			scanf("%d/%d", &year, &month);
-			rewind(stdin); //Can rewind be used?
-			if (month > 12 || month <= 0)
-				printf("\nERROR: Invalid month input. Please try again!\n");
-		} while (month > 12 || month <= 0);
+    do{
+        do{
+            printf("\n\n\t\t\t\t\t***Housing Loan Calculator***\n\n\n");
+            *_string = "\t\tCost of House (RM)\t: \0"; //test implementation of new function
+            dchecker(100000000, 1, 2, 0);
+            cost = _gfloat;
+            //printf("\t\tCost of House (RM)\t: ");
+            //scanf("%lf", &cost);
+            printf("\n\t\tLoan Percentage (%%)\t: ");
+            scanf("%f", &loan_percnt);
+            printf("\n\t\tLoan Tenure (Years)\t: ");
+            scanf("%d", &tenure);
+            printf("\n\t\tInterest Rate (%% P.A)\t: ");
+            scanf("%f", &interest);
+            do{
+                printf("\n\t\tFirst installment (yy/mm) : "); //suggestion - break month and date into two scanf, might make validation easy
+                scanf("%d/%d", &year, &month);
+                rewind(stdin); //Can rewind be used?
+                if (month > 12 || month <= 0)
+                    printf("\nERROR: Invalid month input. Please try again!\n");
+            }while (month > 12 || month <= 0);
 
 
-		//Validation needed for division by zero
-		//Koh: Validation can be done on input stage, verify?
-		loan_percnt *= 0.01;
-		tenure *= 12;
-		loan_amt = loan_percnt * cost;
-		monthly_intrst = ((interest) / 100) / 12;
-		pow_func = pow((1 + monthly_intrst), tenure);
-		installment = loan_amt * (monthly_intrst / (1 - (1 / pow_func)));
+            //Validation needed for division by zero
+            //Koh: Validation can be done on input stage, verify?
+            loan_percnt *= 0.01;
+            tenure *= 12;
+            loan_amt = loan_percnt * cost;
+            monthly_intrst = ((interest) / 100) / 12;
+            pow_func = pow((1 + monthly_intrst), tenure);
+            installment = loan_amt * (monthly_intrst / (1 - (1 / pow_func)));
 
-		
-		printf("\n\t\t**Monthly repayment**\t: RM %.2f\n\n", installment); //Need help for ":" indentation - Koh: \t should have worked?
-		printf("\nProceed to full repayment schedue? (1 - Yes ; 0 - No [Recalculate house loan] ) : ");
-		scanf("%d", &cont_exit);
-	} while (cont_exit == 0);
-		system("cls");
 
-		int no = 0;
-		double balance = loan_amt, intrst_sum = 0;
-		float intrst_accrued, princpl;
-		if (cont_exit == 1)
-		{
-			printf("\n\nLoading...\n\n");
-			Sleep(1200);
-			system("cls");
-			printf("\n\nMonthly Installment Schedule\n----------------------------\n");
-			printf("\n\n   \t \t\tPayable  \tInterest      \t Interest \tPrincipal      \tBalance\n");
-			printf("No.\t Date\t\tDue (RM) \tAccrued (RM)  \t Sum (RM) \t(RM)           \tDue (RM)\n");
-			printf("-------------------------------------------------------------------------------------------------\n");
-			while (no != tenure)
-			{
-				no++;
-				intrst_accrued = monthly_intrst * balance;
-				princpl = installment - intrst_accrued;
-				intrst_sum += intrst_accrued;
-				balance -= princpl;
-				if (month > 12)
-				{
-					year++;
-					month = 1;
-				}
-				else if (balance <= 0)
-					balance = 0;
-				printf("%3d\t%.2d/%d\t\t%.2f\t\t%7.2f\t\t%9.2lf\t%7.2f\t\t%9.2lf\n", no, month, year, installment, intrst_accrued, intrst_sum, princpl, balance);
-				month++;
-			}
-			printf("\n\nChoose any one option to continue? (0 - Return to main menu; 1 - Calculate another housing loan;\n 2 - Exit the program) : ");
-			scanf("%d", &cont_exit);
-			if (cont_exit == 0)
-			{
-				system("cls"); 
-			}
-			else if (cont_exit == 1) //To be discussed
-			{
-				system("cls");
-				//housing_loan(); 
-			}
-			else if (cont_exit == 2)
-				exit(0);
-			else
-			{
-				printf("Error!!!\n\n"); //Anyone is welcome to change the error message.
-				exit(0);
-			}
-		}
+            printf("\n\t\t**Monthly repayment**\t: RM %.2f\n\n", installment); //Need help for ":" indentation - Koh: \t should have worked?
+            printf("\nProceed to full repayment schedue? (1 - Yes ; 0 - No [Recalculate house loan] ) : ");
+            scanf("%d", &cont_exit);
+
+        }while (cont_exit == 0);
+
+        system("cls");
+
+        int no = 0;
+        double balance = loan_amt, intrst_sum = 0;
+        float intrst_accrued, princpl;
+        if (cont_exit == 1){
+            printf("\n\nLoading...\n\n");
+            Sleep(1200);
+            system("cls");
+            printf("\n\nMonthly Installment Schedule\n----------------------------\n");
+            printf("\n\n   \t \t\tPayable  \tInterest      \t Interest \tPrincipal      \tBalance\n");
+            printf("No.\t Date\t\tDue (RM) \tAccrued (RM)  \t Sum (RM) \t(RM)           \tDue (RM)\n");
+            printf("-------------------------------------------------------------------------------------------------\n");
+            while (no != tenure){
+                no++;
+                intrst_accrued = monthly_intrst * balance;
+                princpl = installment - intrst_accrued;
+                intrst_sum += intrst_accrued;
+                balance -= princpl;
+                if (month > 12){
+                    year++;
+                    month = 1;
+                }else if (balance <= 0)
+                    balance = 0;
+                printf("%3d\t%.2d/%d\t\t%.2f\t\t%7.2f\t\t%9.2lf\t%7.2f\t\t%9.2lf\n", no, month, year, installment, intrst_accrued, intrst_sum, princpl, balance);
+                month++;
+            }
+
+            printf("\n\nChoose any one option to continue? (0 - Calculate another housing loan; 1 - Return to main menu;\n 2 - Exit the program) : ");
+            scanf("%d", &cont_exit);
+
+            if(cont_exit == 0){
+                system("cls");
+            }else if (cont_exit == 1){
+                system("cls");
+            }else if (cont_exit == 2){
+                exit(0);
+            }else{
+                printf("Error!!!\n\n"); //Anyone is welcome to change the error message.
+                exit(0);
+            }
+        }
+    }while(cont_exit == 0);
 }
 
-/*int bank_interest(void)
+int bank_interest(void)
 {
     int years, type, cont_exit, freq;
     float initial_amount, monthly_deposit, annual_interest, final_amount, rate;
@@ -188,7 +182,7 @@ void housing_loan(void)
     final_amount = (initial_amount * (pow((1 + rate / 100 / freq),(freq * years)))) + (monthly_deposit * (((pow((1 + rate / 100 / freq),(freq * years))) - 1) / (rate / 100 / freq)));
     printf("\n\t\tFinal savings balance -> RM %.2f\n\n", final_amount);
 
-    /**printf("\n\t\t\tType of interest:\n\n\t\t\t   1 - Annually\n\t\t\t   2 - Monthly\n\t\t\t   3 - Quarterly\n\t\t\t   4 - Semi-annually\n\nPlease key in your selection -> ");
+    printf("\n\t\t\tType of interest:\n\n\t\t\t   1 - Annually\n\t\t\t   2 - Monthly\n\t\t\t   3 - Quarterly\n\t\t\t   4 - Semi-annually\n\nPlease key in your selection -> ");
     scanf("%d", &type);
 
     switch(type)
@@ -257,10 +251,10 @@ void housing_loan(void)
 }
 
     return 0;
-}**/
+}
 
-/*int car_loan(void)
-{	
+int car_loan(void)
+{
 	int loan_period, count=0, year, month;
 	double price, downpay, interest, payment;
 	float rate;
@@ -286,7 +280,7 @@ void housing_loan(void)
 	payment = (((price - downpay) * (rate / 100) * loan_period) + price) / (loan_period * 12);
 	printf("\n\t\t Monthly Repayment (RM)\t %.2lf ", payment);
 	printf("\n\nMonthly Installment Schedule\n----------------------------\n");
-	
+
 	double balance = (price + (price * (rate/100) * loan_period)), principal = price;
 	{
 		printf("\n\n   \t \t\tPayable  \tInterest     \t               \tBalance\n");
@@ -310,6 +304,6 @@ void housing_loan(void)
 			month++;
 		}
 	}
-	
+
 	return 0;
-}*/
+}
--- src/version.h
+++ src/version.h
@@ -8,17 +8,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 105;
-	static const long REVISION  = 588;
+	static const long BUILD  = 112;
+	static const long REVISION  = 630;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 192;
-	#define RC_FILEVERSION 0,1,105,588
-	#define RC_FILEVERSION_STRING "0, 1, 105, 588\0"
-	static const char FULLVERSION_STRING [] = "0.1.105.588";
+	static const long BUILDS_COUNT  = 202;
+	#define RC_FILEVERSION 0,1,112,630
+	#define RC_FILEVERSION_STRING "0, 1, 112, 630\0"
+	static const char FULLVERSION_STRING [] = "0.1.112.630";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 5;
+	static const long BUILD_HISTORY  = 12;
 	
 
 #endif //VERSION_H
