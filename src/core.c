e07ff8e14179cf76363f7546887630a25be6140c
Koh Jun Dong <KohComputer@gmail.com>
2018-11-20 00:30:42 +0800

    Fix misdeclared functiongs and formating the codes

--- src/core.c
+++ src/core.c
@@ -32,59 +32,92 @@ int dchecker(double max, double min, int type, int allowzero)
 {
     char* input[40];
     int error = 1;
-    while(error == 1){
+    while(error == 1)
+    {
         printf("%s",*_string);
         scanf("%s",input);
-        if(type == 1){ //type 1 = integer
+        if(type == 1)  //type 1 = integer
+        {
             long value;
             value = strtol(input,NULL,10);
-            if(value == 0 && allowzero == 0){
+            if(value == 0 && allowzero == 0)
+            {
                 printf("\nValue detected is 0, possible input error. Please try again.\n\n");
-            }else if(value == 0){
+            }
+            else if(value == 0)
+            {
                 printf("\nValue detected is 0, but 0 is allowed. Possible input error?\n\n");
                 error = 0;
                 _gint = value;
-            }else if(errno == ERANGE || value < min || value > max){
+            }
+            else if(errno == ERANGE || value < min || value > max)
+            {
                 printf("\nValue keyed in is out of range, please try again.\n\n");
-            }else{
+            }
+            else
+            {
                 error = 0;
                 _gint = value;
             }
-        }else if(type == 2){ //type 2 = float
+        }
+        else if(type == 2)   //type 2 = float
+        {
             float value;
             value = strtof(input,NULL);
-            if(value == 0 && allowzero == 0){
+            if(value == 0 && allowzero == 0)
+            {
                 printf("\nValue detected is 0, possible input error. Please try again.\n\n");
-            }else if(value == NAN || value == INFINITY){
+            }
+            else if(value == NAN || value == INFINITY)
+            {
                 printf("\nValue is invalid, please try again.\n\n");
-            }else if(value == 0){
+            }
+            else if(value == 0)
+            {
                 printf("\nValue detected is 0, but 0 is allowed. Possible input error?\n\n");
                 error = 0;
                 _gfloat = value;
-            }else if(errno == ERANGE || value < min || value > max){
+            }
+            else if(errno == ERANGE || value < min || value > max)
+            {
                 printf("\nValue keyed in is out of range, please try again.\n\n");
-            }else{
+            }
+            else
+            {
                 error = 0;
                 _gfloat = value;
             }
-        }else if(type == 3){ //type 3 = double
+        }
+        else if(type == 3)   //type 3 = double
+        {
             double value;
             value = strtod(input, NULL);
-            if(value == 0 && allowzero == 0){
+            if(value == 0 && allowzero == 0)
+            {
                 printf("\nValue detected is 0, possible input error. Please try again.\n\n");
-            }else if(value == NAN || value == INFINITY){
+            }
+            else if(value == NAN || value == INFINITY)
+            {
                 printf("\nValue is invalid, please try again.\n\n");
-            }else if(value == 0){
+            }
+            else if(value == 0)
+            {
                 printf("\nValue detected is 0, but 0 is allowed. Possible input error?\n\n");
                 error = 0;
                 _gdouble = value;
-            }else if(errno == ERANGE || value < min || value > max){
+            }
+            else if(errno == ERANGE || value < min || value > max)
+            {
                 printf("\nValue keyed in is out of range, please try again.\n\n");
-            }else{
+            }
+            else
+            {
                 error = 0;
                 _gdouble = value;
             }
-        }else{
+        }
+        else
+        {
             printf("Someone screwed up, check the code.\n\n");
             return 2;
         }
@@ -104,42 +137,43 @@ int dchecker(double max, double min, int type, int allowzero)
 
 void month_function(int nmonth)
 {
-    switch(nmonth){
-        case 1:
-            *_cmonth = "Jan\0";
-            break;
-        case 2:
-            *_cmonth = "Feb\0";
-            break;
-        case 3:
-            *_cmonth = "Mac\0";
-            break;
-        case 4:
-            *_cmonth = "Apr\0";
-            break;
-        case 5:
-            *_cmonth = "May\0";
-            break;
-        case 6:
-            *_cmonth = "Jun\0";
-            break;
-        case 7:
-            *_cmonth = "July\0";
-            break;
-        case 8:
-            *_cmonth = "Aug\0";
-            break;
-        case 9:
-            *_cmonth = "Sep\0";
-            break;
-        case 10:
-            *_cmonth = "Oct\0";
-            break;
-        case 11:
-            *_cmonth = "Nov\0";
-            break;
-        case 12:
-            *_cmonth = "Dec\0";
-            break;
+    switch(nmonth)
+    {
+    case 1:
+        *_cmonth = "Jan\0";
+        break;
+    case 2:
+        *_cmonth = "Feb\0";
+        break;
+    case 3:
+        *_cmonth = "Mac\0";
+        break;
+    case 4:
+        *_cmonth = "Apr\0";
+        break;
+    case 5:
+        *_cmonth = "May\0";
+        break;
+    case 6:
+        *_cmonth = "Jun\0";
+        break;
+    case 7:
+        *_cmonth = "July\0";
+        break;
+    case 8:
+        *_cmonth = "Aug\0";
+        break;
+    case 9:
+        *_cmonth = "Sep\0";
+        break;
+    case 10:
+        *_cmonth = "Oct\0";
+        break;
+    case 11:
+        *_cmonth = "Nov\0";
+        break;
+    case 12:
+        *_cmonth = "Dec\0";
+        break;
     }
 }
--- src/main.c
+++ src/main.c
@@ -18,7 +18,8 @@ double _gdouble;
 void main_menu(void);
 void housing_loan(void);
 void income_tax(void);
-int bank_interest(void);
+void bank_interest(void);
+void ROI(void);
 int car_loan(void);
 
 int main()
@@ -57,7 +58,7 @@ int main()
 
 void main_menu(void)
 {
-	printf("\n\n\t\t\t\t\t***SIMPLE FINANCIAL CALCULATOR***\n\n");
+    printf("\n\n\t\t\t\t\t***SIMPLE FINANCIAL CALCULATOR***\n\n");
     printf("\t\t\t 1  Housing Loan\n");
     printf("\t\t\t 2  Car Loan\n");
     printf("\t\t\t 3  Retirement Planning (KWSP)\n");
@@ -73,53 +74,62 @@ void income_tax(void)
 
 void housing_loan(void)
 {
-	int tenure, month, year, cont_exit;
+    int tenure, month, year, cont_exit;
     double cost, loan_amt, pow_func;
     float loan_percnt, interest, installment, monthly_intrst;
-	system("cls");
+    system("cls");
 
-    do{
-        printf("\n\n\t\t\t\t\t***Housing Loan Calculator***\n\n\n");
-        *_string = "\t\tCost of House (RM)\t: \0"; //test implementation of new function
-        dchecker(100000000, 1, 2, 0);
-        cost = _gfloat;
-        //printf("\t\tCost of House (RM)\t: ");
-        //scanf("%lf", &cost);
-        printf("\n\t\tLoan Percentage (%%)\t: ");
-        scanf("%f", &loan_percnt);
-        printf("\n\t\tLoan Tenure (Years)\t: ");
-        scanf("%d", &tenure);
-        printf("\n\t\tInterest Rate (%% P.A)\t: ");
-        scanf("%f", &interest);
-        do{
-            printf("\n\t\tFirst installment (yy/mm) : "); //suggestion - break month and date into two scanf, might make validation easy
-            scanf("%d/%d", &year, &month);
-            rewind(stdin); //Can rewind be used?
-            if (month > 12 || month <= 0)
-                printf("\nERROR: Invalid month input. Please try again!\n");
-        }while (month > 12 || month <= 0);
-
-
-        //Validation needed for division by zero
-        //Koh: Validation can be done on input stage, verify?
-        loan_percnt *= 0.01;
-        tenure *= 12;
-        loan_amt = loan_percnt * cost;
-        monthly_intrst = ((interest) / 100) / 12;
-        pow_func = pow((1 + monthly_intrst), tenure);
-        installment = loan_amt * (monthly_intrst / (1 - (1 / pow_func)));
+    do
+    {
+        do
+        {
+            printf("\n\n\t\t\t\t\t***Housing Loan Calculator***\n\n\n");
+            *_string = "\t\tCost of House (RM)\t: \0"; //test implementation of new function
+            dchecker(100000000, 1, 2, 0);
+            cost = _gfloat;
+            //printf("\t\tCost of House (RM)\t: ");
+            //scanf("%lf", &cost);
+            printf("\n\t\tLoan Percentage (%%)\t: ");
+            scanf("%f", &loan_percnt);
+            printf("\n\t\tLoan Tenure (Years)\t: ");
+            scanf("%d", &tenure);
+            printf("\n\t\tInterest Rate (%% P.A)\t: ");
+            scanf("%f", &interest);
+            do
+            {
+                printf("\n\t\tFirst installment (yy/mm) : "); //suggestion - break month and date into two scanf, might make validation easy
+                scanf("%d/%d", &year, &month);
+                rewind(stdin); //Can rewind be used?
+                if (month > 12 || month <= 0)
+                    printf("\nERROR: Invalid month input. Please try again!\n");
+            }
+            while (month > 12 || month <= 0);
 
 
-        printf("\n\t\t**Monthly repayment**\t: RM %.2f\n\n", installment); //Need help for ":" indentation - Koh: \t should have worked?
-        printf("\nProceed to full repayment schedue? (1 - Yes ; 0 - No [Recalculate house loan] ) : ");
-        scanf("%d", &cont_exit);
+            //Validation needed for division by zero
+            //Koh: Validation can be done on input stage, verify?
+            loan_percnt *= 0.01;
+            tenure *= 12;
+            loan_amt = loan_percnt * cost;
+            monthly_intrst = ((interest) / 100) / 12;
+            pow_func = pow((1 + monthly_intrst), tenure);
+            installment = loan_amt * (monthly_intrst / (1 - (1 / pow_func)));
+
+
+            printf("\n\t\t**Monthly repayment**\t: RM %.2f\n\n", installment); //Need help for ":" indentation - Koh: \t should have worked?
+            printf("\nProceed to full repayment schedue? (1 - Yes ; 0 - No [Recalculate house loan] ) : ");
+            scanf("%d", &cont_exit);
+
+        }
+        while (cont_exit == 0);
 
         system("cls");
 
         int no = 0;
         double balance = loan_amt, intrst_sum = 0;
         float intrst_accrued, princpl;
-        if (cont_exit == 1){
+        if (cont_exit == 1)
+        {
             printf("\n\nLoading...\n\n");
             Sleep(1200);
             system("cls");
@@ -127,16 +137,19 @@ void housing_loan(void)
             printf("\n\n   \t \t\tPayable  \tInterest      \t Interest \tPrincipal      \tBalance\n");
             printf("No.\t Date\t\tDue (RM) \tAccrued (RM)  \t Sum (RM) \t(RM)           \tDue (RM)\n");
             printf("-------------------------------------------------------------------------------------------------\n");
-            while (no != tenure){
+            while (no != tenure)
+            {
                 no++;
                 intrst_accrued = monthly_intrst * balance;
                 princpl = installment - intrst_accrued;
                 intrst_sum += intrst_accrued;
                 balance -= princpl;
-                if (month > 12){
+                if (month > 12)
+                {
                     year++;
                     month = 1;
-                }else if (balance <= 0)
+                }
+                else if (balance <= 0)
                     balance = 0;
                 printf("%3d\t%.2d/%d\t\t%.2f\t\t%7.2f\t\t%9.2lf\t%7.2f\t\t%9.2lf\n", no, month, year, installment, intrst_accrued, intrst_sum, princpl, balance);
                 month++;
@@ -145,21 +158,29 @@ void housing_loan(void)
             printf("\n\nChoose any one option to continue? (0 - Calculate another housing loan; 1 - Return to main menu;\n 2 - Exit the program) : ");
             scanf("%d", &cont_exit);
 
-            if(cont_exit == 0){
+            if(cont_exit == 0)
+            {
                 system("cls");
-            }else if (cont_exit == 1){
+            }
+            else if (cont_exit == 1)
+            {
                 system("cls");
-            }else if (cont_exit == 2){
+            }
+            else if (cont_exit == 2)
+            {
                 exit(0);
-            }else{
+            }
+            else
+            {
                 printf("Error!!!\n\n"); //Anyone is welcome to change the error message.
                 exit(0);
             }
         }
-    }while(cont_exit == 0);
+    }
+    while(cont_exit == 0);
 }
 
-int bank_interest(void)
+void bank_interest(void)
 {
     int years, type, cont_exit, freq;
     float initial_amount, monthly_deposit, annual_interest, final_amount, rate;
@@ -183,7 +204,7 @@ int bank_interest(void)
     scanf("%d", &type);
 
     switch(type)
-    // [P(1+r/n)^(nt)] + [PMT x (((1+r/n)^(nt)-1)/(r/n))]
+        // [P(1+r/n)^(nt)] + [PMT x (((1+r/n)^(nt)-1)/(r/n))]
     {
     case 1:
         final_amount = (initial_amount * (pow((1 + rate / 100 / 1),(1 * years)))) + (monthly_deposit * (((pow((1 + rate / 100 / 1),(1 * years))) - 1) / (rate / 100 / 1)));
@@ -217,90 +238,87 @@ int bank_interest(void)
         printf("Error!!!\n\n"); //Anyone is welcome to change the error message.
         exit(0);
     }
-    int ROI (void)
-{
-	//1- Decalaration
-	float amt_invested, amt_returned, investment_time, investment_length, investment_gain, percentage_return, profit, invested ;
-
-	//2- Input
-	printf("Amount Invested :RM");
-	scanf("%f" , &amt_invested);
-
-	printf("Amount Returned :RM");
-	scanf("%f" , &amt_returned);
-
-	printf("Investment Length (years):");
-	scanf("%f" , &investment_time);
-
-	//3- Calculation
-	investment_gain = amt_returned - amt_invested;
-	percentage_return = ((amt_returned - amt_invested)/amt_invested) * 100;
-	profit = (investment_gain / amt_returned) *100;
-	invested = (amt_invested/ amt_returned) *100;
-
-
-	//4- Output
-	printf("Investment Gain: RM%.2f \n", investment_gain);
-	printf("Return of Investment: %.2f%% \n", percentage_return);
-	printf("Percentage of Profit: %.2f%% \n", profit);
-	printf("Percentage of Invested: %.2f%% \n", invested);
-	printf("Investment Length :%.1f years \n", investment_time);
 }
 
-    return 0;
+void ROI (void)
+{
+    //1- Decalaration
+    float amt_invested, amt_returned, investment_time, investment_gain, percentage_return, profit, invested ;
+
+    //2- Input
+    printf("Amount Invested :RM");
+    scanf("%f", &amt_invested);
+
+    printf("Amount Returned :RM");
+    scanf("%f", &amt_returned);
+
+    printf("Investment Length (years):");
+    scanf("%f", &investment_time);
+
+    //3- Calculation
+    investment_gain = amt_returned - amt_invested;
+    percentage_return = ((amt_returned - amt_invested)/amt_invested) * 100;
+    profit = (investment_gain / amt_returned) *100;
+    invested = (amt_invested/ amt_returned) *100;
+
+
+    //4- Output
+    printf("Investment Gain: RM%.2f \n", investment_gain);
+    printf("Return of Investment: %.2f%% \n", percentage_return);
+    printf("Percentage of Profit: %.2f%% \n", profit);
+    printf("Percentage of Invested: %.2f%% \n", invested);
+    printf("Investment Length :%.1f years \n", investment_time);
 }
 
-int car_loan(void)
+void car_loan(void)
 {
-	int loan_period, count=0, year, month;
-	double price, downpay, interest, payment;
-	float rate;
-	printf("\n\n\t\t\t\t\t***Car Loan Calculator***\n\n\n");
-	printf("\t\tCar Price (RM)\t: ");
-	scanf("%lf", &price);
-	printf("\n\t\tDownpayment (RM)\t: ");
-	scanf("%lf", &downpay);
-	printf("\n\t\tLoan Period (Years)\t: ");
-	scanf("%d", &loan_period);
-	printf("\n\t\tInterest Rate (%% P.A)\t: ");
-	scanf("%f", &rate);
-	printf("\n\t\tFirst loan payback (mm/yy)\t: "); //suggestion - break month and date into two scanf, might make validation easy
-	scanf("%d/%d", &year, &month);
-	if (month > 12 || month <= 0)
-	{
-		printf("\nERROR: Invalid month input. Please try again!");
-		printf("\n\n\t\tFirst loan payback (yy/mm) : ");
-		scanf("%d/%d", &year, &month);
-	}
+    int loan_period, count=0, year, month;
+    double price, downpay, interest, payment;
+    float rate;
+    printf("\n\n\t\t\t\t\t***Car Loan Calculator***\n\n\n");
+    printf("\t\tCar Price (RM)\t: ");
+    scanf("%lf", &price);
+    printf("\n\t\tDownpayment (RM)\t: ");
+    scanf("%lf", &downpay);
+    printf("\n\t\tLoan Period (Years)\t: ");
+    scanf("%d", &loan_period);
+    printf("\n\t\tInterest Rate (%% P.A)\t: ");
+    scanf("%f", &rate);
+    printf("\n\t\tFirst loan payback (mm/yy)\t: "); //suggestion - break month and date into two scanf, might make validation easy
+    scanf("%d/%d", &year, &month);
+    if (month > 12 || month <= 0)
+    {
+        printf("\nERROR: Invalid month input. Please try again!");
+        printf("\n\n\t\tFirst loan payback (yy/mm) : ");
+        scanf("%d/%d", &year, &month);
+    }
 
 
-	payment = (((price - downpay) * (rate / 100) * loan_period) + price) / (loan_period * 12);
-	printf("\n\t\t Monthly Repayment (RM)\t %.2lf ", payment);
-	printf("\n\nMonthly Installment Schedule\n----------------------------\n");
+    payment = (((price - downpay) * (rate / 100) * loan_period) + price) / (loan_period * 12);
+    printf("\n\t\t Monthly Repayment (RM)\t %.2lf ", payment);
+    printf("\n\nMonthly Installment Schedule\n----------------------------\n");
 
-	double balance = (price + (price * (rate/100) * loan_period)), principal = price;
-	{
-		printf("\n\n   \t \t\tPayable  \tInterest     \t               \tBalance\n");
-		printf("No.\t Date\t\tDue (RM) \tAccrued (RM) \tPrincipal (RM) \tDue (RM)\n");
-		printf("------------------------------------------------------------------------------------\n");
+    double balance = (price + (price * (rate/100) * loan_period)), principal = price;
+    {
+        printf("\n\n   \t \t\tPayable  \tInterest     \t               \tBalance\n");
+        printf("No.\t Date\t\tDue (RM) \tAccrued (RM) \tPrincipal (RM) \tDue (RM)\n");
+        printf("------------------------------------------------------------------------------------\n");
 
-		while (count != loan_period * 12)
-		{
-			count++;
-			balance -= payment;
-			interest = (price * (rate / 100) * loan_period)/(loan_period * 12);
-			principal = payment - interest;
-			if (month > 12)
-			{
-				year++;
-				month = 1;
-			}
-			if (balance < 0)
-				balance = 0;
-			printf("%3d\t%d/%.2d\t\t%.2f\t\t%7.2f\t\t%9.2lf\t%.2f\n", count, year, month, payment, interest, principal, balance);
-			month++;
-		}
-	}
-
-	return 0;
+        while (count != loan_period * 12)
+        {
+            count++;
+            balance -= payment;
+            interest = (price * (rate / 100) * loan_period)/(loan_period * 12);
+            principal = payment - interest;
+            if (month > 12)
+            {
+                year++;
+                month = 1;
+            }
+            if (balance < 0)
+                balance = 0;
+            printf("%3d\t%d/%.2d\t\t%.2f\t\t%7.2f\t\t%9.2lf\t%.2f\n", count, year, month, payment, interest, principal, balance);
+            month++;
+        }
+    }
 }
