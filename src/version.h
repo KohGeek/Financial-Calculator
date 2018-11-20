0918581c6b28e3db50921482855ce323db343eaf
Koh Jun Dong <KohComputer@gmail.com>
2018-11-21 01:15:57 +0800

    Increment build to beta version

--- src/version.h
+++ src/version.h
@@ -2,20 +2,20 @@
 #define VERSION_H
 
 	//Software Status
-	static const char STATUS[] =  "Alpha";
-	static const char STATUS_SHORT[] =  "a";
+	static const char STATUS[] =  "Beta";
+	static const char STATUS_SHORT[] =  "b";
 	
 	//Standard Version Type
-	static const long MAJOR  = 0;
-	static const long MINOR  = 2;
-	static const long BUILD  = 218;
+	static const long MAJOR  = 1;
+	static const long MINOR  = 1;
+	static const long BUILD  = 0;
 	static const long REVISION  = 1154;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 472;
-	#define RC_FILEVERSION 0,2,218,1154
-	#define RC_FILEVERSION_STRING "0, 2, 218, 1154\0"
-	static const char FULLVERSION_STRING [] = "0.2.218.1154";
+	static const long BUILDS_COUNT  = 474;
+	#define RC_FILEVERSION 1,1,0,1154
+	#define RC_FILEVERSION_STRING "1, 1, 0, 1154\0"
+	static const char FULLVERSION_STRING [] = "1.1.0.1154";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 18;
