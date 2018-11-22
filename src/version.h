c6c68e64aa8385c9f013168d2decaf2ec47ea6d2
Koh Jun Dong <KohComputer@gmail.com>
2018-11-22 22:51:25 +0800

    Complete overhaul of the datatype checker
    - Bug: Newline and space can't be detected.
    
    - Fix: A lot of sweat, tears and blood.
    
    And using fgets.

--- src/version.h
+++ src/version.h
@@ -9,16 +9,16 @@
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
 	static const long BUILD  = 1;
-	static const long REVISION  = 1545;
+	static const long REVISION  = 1800;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 627;
-	#define RC_FILEVERSION 1,0,1,1545
-	#define RC_FILEVERSION_STRING "1, 0, 1, 1545\0"
-	static const char FULLVERSION_STRING [] = "1.0.1.1545";
+	static const long BUILDS_COUNT  = 668;
+	#define RC_FILEVERSION 1,0,1,1800
+	#define RC_FILEVERSION_STRING "1, 0, 1, 1800\0"
+	static const char FULLVERSION_STRING [] = "1.0.1.1800";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 0;
+	static const long BUILD_HISTORY  = 6;
 	
 
 #endif //VERSION_H
