302bac609d4b31aa6fbdc39dd8d011f31ea96bfb
Koh Jun Dong <KohComputer@gmail.com>
2018-11-23 16:37:56 +0800

    Elevate to Release

--- src/version.h
+++ src/version.h
@@ -2,20 +2,20 @@
 #define VERSION_H
 
 	//Software Status
-	static const char STATUS[] =  "Release Candidate";
-	static const char STATUS_SHORT[] =  "rc";
+	static const char STATUS[] =  "Release";
+	static const char STATUS_SHORT[] =  "r";
 	
 	//Standard Version Type
 	static const long MAJOR  = 1;
-	static const long MINOR  = 0;
-	static const long BUILD  = 14;
+	static const long MINOR  = 1;
+	static const long BUILD  = 0;
 	static const long REVISION  = 2305;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 694;
-	#define RC_FILEVERSION 1,0,14,2305
-	#define RC_FILEVERSION_STRING "1, 0, 14, 2305\0"
-	static const char FULLVERSION_STRING [] = "1.0.14.2305";
+	#define RC_FILEVERSION 1,1,0,2305
+	#define RC_FILEVERSION_STRING "1, 1, 0, 2305\0"
+	static const char FULLVERSION_STRING [] = "1.1.0.2305";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 19;
