88c0346dba14e9130ad6ceadf727ad4cdc89772e
Koh Jun Dong <KohComputer@gmail.com>
2018-11-30 04:34:38 +0800

    Update version

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-/** 
+/**
  * This part of the code was intended only for autoversioing and
  * logging purpose, a function provided as-is by Code:Blocks.
 **/
@@ -9,21 +9,21 @@
 	//Software Status
 	static const char STATUS[] =  "Release";
 	static const char STATUS_SHORT[] =  "r";
-	
+
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1;
-	static const long REVISION  = 2387;
-	
+	static const long BUILD  = 2;
+	static const long REVISION  = 2563;
+
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 695;
-	#define RC_FILEVERSION 1,1,1,2387
-	#define RC_FILEVERSION_STRING "1, 1, 1, 2387\0"
-	static const char FULLVERSION_STRING [] = "1.1.1.2387";
-	
+	static const long BUILDS_COUNT  = 698;
+	#define RC_FILEVERSION 1,1,2,2563
+	#define RC_FILEVERSION_STRING "1, 1, 2, 2563\0"
+	static const char FULLVERSION_STRING [] = "1.1.2.2563";
+
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 20;
-	
+	static const long BUILD_HISTORY  = 23;
+
 
 #endif //VERSION_H
